4798e55e7f76edb3b2824e8b8fe68b6789dd9e0f
My Mai <my.mai@asnet.com.vn>
2015-04-23 21:42:03 +0700

    Config environment

--- app-car/leoleo/config/environment.rb
+++ app-car/leoleo/config/environment.rb
@@ -3,7 +3,9 @@ require 'bundler/setup'
 require 'lotus/setup'
 require_relative '../lib/leoleo'
 require_relative '../apps/web/application'
+require_relative '../apps/api/v1/application'
 
 Lotus::Container.configure do
+  mount ApiV1::Application, at: '/api/v1'
   mount Web::Application, at: '/'
 end
