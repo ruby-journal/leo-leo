0a32d5a53db7be3ce3dae978b2b30315be54577e
My Mai <my.mai@asnet.com.vn>
2015-04-23 21:38:36 +0700

    Add application.rb

---
+++ app-car/leoleo/apps/api/v1/application.rb
@@ -0,0 +1,25 @@
+module ApiV1
+  class Application < Lotus::Application
+    configure do
+      root __dir__
+
+      body_parsers :json
+      default_format :json
+
+      routes 'config/routes'
+
+      load_paths << [
+        'controllers',
+        'presenters'
+      ]
+
+      controller.prepare do
+        # before callbacks
+      end
+
+      view.prepare do
+        # before callbacks
+      end
+    end
+  end
+end
