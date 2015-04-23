c5e64cddb375d929016801a92ef02e621b134f4d
My Mai <my.mai@asnet.com.vn>
2015-04-23 20:16:55 +0700

    Add Users mapping

--- app-car/leoleo/lib/config/mapping.rb
+++ app-car/leoleo/lib/config/mapping.rb
@@ -1,7 +1,11 @@
-# collection :users do
-#   entity     User
-#   repository UserRepository
-#
-#   attribute :id,   Integer
-#   attribute :name, String
-# end
+collection :users do
+  entity     User
+  repository UserRepository
+
+  attribute :id,   Integer
+  attribute :name, String
+  attribute :email, String
+  attribute :gender, Integer
+  attribute :about, String
+  attribute :image_url, String
+end
