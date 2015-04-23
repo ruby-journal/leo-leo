b72ac58bc875dff013d7e5a0befa95a96d1ee1e3
My Mai <my.mai@asnet.com.vn>
2015-04-23 21:11:11 +0700

    Config mapping

--- app-car/leoleo/lib/leoleo.rb
+++ app-car/leoleo/lib/leoleo.rb
@@ -23,17 +23,8 @@ Lotus::Model.configure do
   #
   # You can specify mapping file to load with:
   #
-  # mapping "#{__dir__}/config/mapping"
+  mapping "#{__dir__}/config/mapping"
   #
   # Alternatively, you can use a block syntax like the following:
   #
-  mapping do
-    # collection :users do
-    #   entity     User
-    #   repository UserRepository
-    #
-    #   attribute :id,   Integer
-    #   attribute :name, String
-    # end
-  end
 end.load!
