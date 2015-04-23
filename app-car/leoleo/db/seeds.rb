24c199ef477483f58ab3c69432802374850aea6c
My Mai <my.mai@asnet.com.vn>
2015-04-23 21:08:44 +0700

    update seeds.rb

--- app-car/leoleo/db/seeds.rb
+++ app-car/leoleo/db/seeds.rb
@@ -1,6 +1,6 @@
 [
   {name: 'My Mai', email: 'my.mai@asnet.com.vn', gender: 0, about: 'cung okay', image_url: 'https://avatars3.githubusercontent.com/u/6791942?v=3&s=460'},
-].each do |car_attrs|
-  car = Car.new(car_attrs)
-  CarRepository.persist(car)
+].each do |user_attrs|
+  user = User.new(user_attrs)
+  UserRepository.persist(user)
 end
